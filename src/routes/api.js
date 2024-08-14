ab34a59c292610a5b36748abd6938ec2ef3eebab
Hoang Le <lehuyhoang2561@gmail.com>
2024-08-13 23:07:04 -0400

    step 5

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createUser } = require("../controllers/userController");
+const { createUser, handleLogin } = require("../controllers/userController");
 
 const routerAPI = express.Router();
 
@@ -7,5 +7,6 @@ routerAPI.get("/", (req, res) => {
   return res.status(200).json("Hello World");
 });
 routerAPI.post("/register", createUser);
+routerAPI.post("/login", handleLogin);
 
 module.exports = routerAPI; //export default
